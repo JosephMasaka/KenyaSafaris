721d151a081eaabd67adfd23f4dfef50cfdbe451
Joss <34286057+JosephMasaka@users.noreply.github.com>
2023-02-10 11:42:19 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './assets/components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
