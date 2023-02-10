3a4de28bea04389898fe6fdf7b9e031bb495603f
Joss <34286057+JosephMasaka@users.noreply.github.com>
2023-02-10 12:20:57 +0300

    Navbar plus styles and simple mods

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 function Navbar() {
   return (
     <div id='Navbar' className='navbar'>
-        
+        <div className='flex flex-row container'>
+            <div className='logo'>
+                <h2 id='logo'>Safari's</h2>
+            </div>
+            <div className='ham-menu'>
+                <div>
+                    <div className="ham ham-1"></div>
+                    <div className="ham ham-2"></div>
+                    <div className="ham ham-3"></div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
